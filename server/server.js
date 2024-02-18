18953cb0b3a0499999caa91e466a095c6e4be346
Meenakshi Sundaram <meenakshisundaram.in@yahoo.com>
2024-02-18 22:40:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,11 +7,41 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(bodyParser.json());
 
 // Connect to MongoDB Atlas
 mongoose.connect('mongodb+srv://nicholasbranson:Karunya8055$@clusterkitsaiml.rydxbrs.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+const hackathonSchema = new mongoose.Schema({
+  batch: Number,
+  event_name: String,
+  organizer: String,
+  event_description: String,
+  hackathon_mode: String,
+  hackathon_location: String,
+  prizes:String,
+  application_deadline: Date,
+  application_link: String
+})
+
+const Hackathon = mongoose.model('Hackathon',hackathonSchema);
+
+app.post('/api/hackathons',async(req,res) => {
+  try{
+    const {batch,event_name,organizer,event_description,hackathon_mode,hackathon_location,prizes,application_deadline,application_link} = req.body; 
+
+    const newHackathon = new Hackathon({batch,event_name,organizer,event_description,hackathon_mode,hackathon_location,prizes,application_deadline,application_link});
+
+    await newHackathon.save();
+    res.status(201).send(newHackathon);
+  }
+  catch(error){
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
 });
 
 // Create Question schema
